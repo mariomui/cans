760643b22ff2d4af80bb78c46e7d319196c93c69
mariomui <yi.kan.mui@gmail.com>
2022-04-06 01:44:41 -0700

    Install execa and listr.

---
+++ plugins/automation.md
@@ -0,0 +1,4 @@
+* execa
+  * used to execute scripts or node child process.
+  * node npm install maybe?
+* listr used to do terminal processes
---
+++ starter.js
@@ -0,0 +1,38 @@
+import { execa } from 'execa';
+import Listr from 'listr';
+
+const tasks = new Listr([
+  {
+    title: 'Git',
+    task: (ctx, task) =>
+      //ctx can be passed in the same Listr, and task allows you to skip
+      execa('git', ['rev-parse', '--is-inside-work-tree']).then(
+        ({ stdout }) => {
+          if (stdout === true) {
+            console.log(typeof result, result);
+          } else {
+            execa('git', ['--init']);
+          }
+        }
+      ),
+  },
+  {
+    title: 'Install package dependencies with npm',
+    // enabled: (ctx) => !!ctx?.npm === false,
+    // only enable if npm context variable is false;
+    task: (ctx, task) =>
+      execa('npm', ['install'])
+        .then(() => {
+          ctx.npm = true;
+          console.log('here');
+        })
+        .catch(() => {
+          ctx.npm = false;
+          task.skip('Npm not available, install it via `npm install -g yarn`');
+        }),
+  },
+]);
+
+tasks.run().catch((err) => {
+  console.error(err);
+});
