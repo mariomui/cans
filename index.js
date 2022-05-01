b54173d0d72dd0da4d66dbd67eeebeee85dc5be1
mariomui <yi.kan.mui@gmail.com>
2022-04-30 22:32:34 -0700

    Update errors on install.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
-import path from 'node:path';
 import minimist from 'minimist';
 import { Plop, run } from 'plop';
 
 const args = process.argv.slice(2);
 const argv = minimist(args);
 
-import { dirname } from 'node:path';
+import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { cansConfig } from './cans-automator/cans.config';
-import path from 'node:path';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const configPath = path.resolve(__dirname, cansConfig.plop.configFilename);
 
