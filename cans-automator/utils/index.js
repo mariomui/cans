d3a132522852df084beb1837120ab0bed9982880
mariomui <yi.kan.mui@gmail.com>
2022-05-02 02:44:02 -0700

    Setup scaffolding for refactoring npm scripts away into a plugin archtecture.

---
+++ cans-automator/utils/index.js
@@ -0,0 +1,19 @@
+const createNodeScriptFactory = (
+  title,
+  command,
+  { posix, thenCb = () => true, catchCb = _catchCb }
+) => {
+  return {
+    title,
+    task: (ctx, task) =>
+      execa(command, posix)
+        // execa('npm', ['install'])
+        .then(thenCb)
+        .catch(catchCb),
+  };
+};
+
+function _catchCb(err) {
+  console.log(err);
+  return true;
+}
